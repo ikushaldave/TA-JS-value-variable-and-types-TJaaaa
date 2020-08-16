a7c1d0badc23375649d2fc746887d1207aea487d
ikushaldave <ikushaldave@gmail.com>
2020-08-16 14:09:19 +0530

    Block 4 Completed

--- block-BJaaay/code/index.js
+++ block-BJaaay/code/index.js
@@ -4,10 +4,63 @@
 
 // Using `console.log` log all the values from 1 to 10.
 
+for (let i = 1; i <= 10; i++) {
+	console.log(i);
+}
+
+let i = 1;
+while (i <= 10) {
+	console.log(i);
+	i++;
+}
+
 // Using `console.log` log all the evan values from 1 to 10.
 
-// Using `console.log` log all the od values from 1 to 10.
+for (let j = 1; j <= 10; j++) {
+	if (j % 2 === 0) {
+		console.log(j);
+	}
+}
+
+let j = 1;
+while (j <= 10) {
+	if (j % 2 === 0) {
+		console.log(j);
+	}
+	j++;
+}
+
+// Using `console.log` log all the odd values from 1 to 10.
+
+for (let k = 1; k <= 10; k++) {
+	if (k % 2 !== 0) {
+		console.log(k);
+	}
+}
+
+let k = 1;
+while (k <= 10) {
+	if (k % 2 !== 0) {
+		console.log(k);
+	}
+	k++;
+}
 
 // Calculate the sum of all numbers from 1 to 10.
 
+let sum = 0;
+
+for (let m = 1; m <= 10; m++) {
+	sum += m;
+}
+
+console.log(sum);
+
 // Log all the values from 1 to 10 using while loop
+
+let n = 1;
+
+while (n <= 10) {
+	console.log(n);
+	n++;
+}
